5f6a1905975fcd10b54530d12124b3c5d9de906a
Nathaniel Tubb <nathaniel@mediumrareinc.com>
2018-09-15 14:43:41 -0700

    bits and bobs

--- 404.php
+++ 404.php
@@ -3,12 +3,12 @@
 
   <section class="error-404 not-found">
 
-    <h1 class="post-title">
-      <i class="fas fa-fw fa-lg fa-unlink"></i><?php esc_html_e( 'Oops! That page can&rsquo;t be found.'); ?>
+    <h1 class="error-404-title">
+      <span aria-hidden="true" class="fa fa-fw fa-lg fa-unlink"></span>&nbsp;Oops!<br>That page can&rsquo;t be found.
     </h1>
 
     <p><em><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links in the main menu or a search?'); ?></em></p>
-    <p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="button"><i class="fas fa-fw fa-home"></i>&nbsp;<?php _e('Go Back to the Homepage') ?></a></p>
+    <p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="button"><span aria-hidden="true" class="fa fa-fw fa-home"></span>&nbsp;<?php _e('Go Back to the Homepage') ?></a></p>
     <?php get_search_form(); ?>
 
   </section>
